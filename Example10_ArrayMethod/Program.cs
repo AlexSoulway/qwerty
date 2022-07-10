0b6d830cce83e550bcc70b73dda9c2016b9919cf
Alex <soullovesthesun@gmail.com>
2022-07-10 19:59:16 +0300

    add Example 10 Array Method

---
+++ Example10_ArrayMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {11, 21, 31, 14, 51, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+       Console.WriteLine(index); 
+       break;    //брейк добавили чтобы остановиться на найденном значении, а не бегать по всему массиву, иначе он показал бы все индексы совпадающие с файнд
+    }
+    //index = index + 1;
+index++;
+}
